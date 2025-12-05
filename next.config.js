7fbc504f675184b946ac23efb93ce7528f721a2e
widescopeindustries <lyndon@widescopeindustries.com>
2025-12-05 12:45:53 -0600

    Fix: Add Google image domains for external listings

--- next.config.js
+++ next.config.js
@@ -6,6 +6,14 @@ const nextConfig = {
         protocol: 'https',
         hostname: '**.supabase.co',
       },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'streetviewpixels-pa.googleapis.com',
+      },
     ],
   },
   async headers() {
